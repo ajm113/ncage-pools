b1720f8ee421838804d5bf6a4c92022ddaed1429
Andrew McRobb <andrewmcrobb@gmail.com>
2017-08-24 20:49:34 -0700

    Added better deployment script for one command pushes.

--- push.sh
+++ push.sh
@@ -1,7 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# Upload our repository
-rsync -r -avz -e "ssh -i Andrew-PC.pem" --delete ./ \
+# Our script variables
+
+HOST="ubuntu@ec2-35-160-159-153.us-west-2.compute.amazonaws.com" # Our remote server for production.
+ROOT_FOLDER=/var/www/                                            # Root folder of where our app sits.
+KEY_FILE="/home/vagrant/Andrew-PC.pem"                           # [CHANGE THIS] Our pem file for SSH connection.
+
+# Pushes our codebase into the remote server. Everything is done in one sweet ssh command! :)
+
+yarn run clean          # We need to cleanup anything that isn't in use before rsync.
+yarn run production     # Make sure all the static content we generate is production ready.
+
+# Upload our app!
+rsync -r -avz -e "ssh -i $KEY_FILE" --delete ./ \
     --exclude push.sh \
     --exclude connect_remote.sh \
     --exclude README.md \
@@ -13,9 +24,7 @@ rsync -r -avz -e "ssh -i Andrew-PC.pem" --delete ./ \
     --exclude Vagrantfile \
     --exclude Andrew-PC.pem \
     --exclude .editorconfig \
-    --exclude node_modules \
-    --exclude .vagrant \
     --exclude .gitignore \
-    --exclude resources/assets/ \
-    --exclude vendor/laravel/framework/.git \
-    ubuntu@ec2-35-160-159-153.us-west-2.compute.amazonaws.com:/var/www/
+    --exclude .git \
+    --exclude node_modules \
+    $HOST:$ROOT_FOLDER
