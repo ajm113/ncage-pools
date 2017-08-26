a103d8bb86eb054c41cea397719b6b6583344acb
Andrew McRobb <andrewmcrobb@gmail.com>
2017-08-25 20:56:49 -0700

    Convert our var to const since es2015 is enabled.

--- resources/assets/js/checkout-page.js
+++ resources/assets/js/checkout-page.js
@@ -3,7 +3,7 @@
     Since in this case we just want to create some nifty JS, not much is required just to get the job done.
 */
 
-var SAME_AS_SHIPPING_CHECKBOX_TOGGLE = '#shipping-same-as-billing';
+const SAME_AS_SHIPPING_CHECKBOX_TOGGLE = '#shipping-same-as-billing';
 
 $(function(){
     $(SAME_AS_SHIPPING_CHECKBOX_TOGGLE).on("change", function() {
